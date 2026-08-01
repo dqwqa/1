0da16d8a38a09d90909a384f14aa968a6ca7ddfd
dqwqa <15862845902@163.com>
2026-08-01 13:34:52 +0800

    feat: add watcher entrypoint bootstrap

---
+++ watcher.mjs
@@ -0,0 +1,3 @@
+// placeholder: full watcher implementation is being finalized in next commit
+export const WATCH_ITEMS = ['国王球','棱镜球','镜面相框','炫彩蛋','炫彩精灵蛋','首领血脉秘药','祝福项坠'];
+console.log(JSON.stringify({status:'bootstrap',watchItems:WATCH_ITEMS}));
